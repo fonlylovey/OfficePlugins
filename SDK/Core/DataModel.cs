110e4bf26fd8a4a7b0541262d8b7445871673cf2
Simon <fonlylovey@163.com>
2020-03-26 21:09:16 +0800

    补传文件

---
+++ SDK/Core/DataModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public class ResourceData
+    {
+        public String ID { set; get; } = String.Empty;
+        public String Name { set; get; } = String.Empty;
+        public String Label { set; get; } = String.Empty;
+        public String IconUrl { set; get; } = String.Empty;
+        public String FileUrl { set; get; } = String.Empty;
+    }
+
+    public class ResourceModel
+    {
+        public int PageCount { set; get; } = 0;
+
+        public List<ResourceData> ResourceList = new List<ResourceData>();
+    }
+
+    public enum ResourceType
+    {
+        Template,
+        Icon,
+        legend
+    }
+}
