9e7761d0994d28835fb530a56cf13eee7d51436e
Simon <fonlylovey@163.com>
2020-04-05 16:28:12 +0800

    添加登陆界面

---
+++ ReportAssist/UI/LoginWidget.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CustomControls;
+
+namespace PPTPlugin
+{
+    public partial class LoginWidget : CustomForm
+    {
+        public LoginWidget()
+        {
+            InitializeComponent();
+        }
+    }
+}
